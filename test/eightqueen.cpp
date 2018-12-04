b13afaa5c76934ff90b3094f70ede56122ac0056
tearsforyears <1027664894@qq.com>
2018-12-04 11:31:00 +0800

    eight queen

---
+++ test/eightqueen.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+#define n 8
+int arr[n]={-1,-1,-1,-1,-1,-1,-1,-1};
+//this array store the position of queen
+//eg: arr[0]=4 arr[1]=7 means (4,0) and (7,1) both has a queen
+bool judge(int y,int x){
+    // false for do not use the (x,y) 
+	for(int i=0;i<y;i++){
+        if(x==arr[i])return false;
+        if((y-i)==(x-arr[i]))return false;
+        if((y-i)+(x-arr[i])==0)return false;
+    }
+    return true;
+}
+void dfs(int y){
+	for(int x=0;x<n;x++){
+		if(y<n){
+			if(judge(y,x)){//pruning
+				arr[y]=x;
+				dfs(y+1);
+				arr[y]=-1;//back-track			
+			}
+		}else{
+			for(int i=0;i<n;i++)cout<<arr[i]<<",";
+			cout<<endl;
+			return;
+		}
+	}
+	return;
+}
+int main(){
+	dfs(0);
+	return 0;
+} 
